55b1902204ded9dd045e19af0bd40d349ee0ba91
Richard Brant - Aluno Unidavi <richard.brant@unidavi.edu.br>
2025-09-25 20:36:16 -0300

    código feito!!

---
+++ aula_08/index.php
@@ -0,0 +1,70 @@
+<?php
+echo 'Prática 1';
+echo '<br>';
+echo '<br>';
+// Pratica 1
+//Function
+function echoVars(string $var1, string $var2){
+    echo $var1 . " " . $var2;  
+}
+// Programa principal
+
+$var1 = "Richard";
+$var2 = "Brant";
+
+echoVars($var1, $var2);
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+echo 'Prática 2';
+echo '<br>';
+echo '<br>';
+// Pratica 2
+// Criar um programa em PHP para:
+// Declarar duas variáveis com os nomes: SALARIO1 e
+// SALARIO2;
+// Atribuir 1000 para a variável SALARIO1;
+// Atribuir 2000 para a variável SALARIO2;
+// Atribuir o valor da variável SALARIO1 para SALARIO2
+// Incrementar 1 na variável SALARIO2;
+// Adicionar 10% de aumento para SALARIO1;
+// Produzir uma saída com o texto: “Valor Salário 1: XX e Valor
+// Salário: XX”;
+
+$iSalario1 = 1000;
+$iSalario2 = 2000;
+
+$iSalario2 = $iSalario1;
+$iSalario2++;
+$iSalario1 *= 1.1;
+
+echo "Valor Salário 1: $iSalario1 e Valor Salário 2: $iSalario2";
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+echo 'Prática 3';
+echo '<br>';
+echo '<br>';
+
+// Utilizando o mesmo programa criado na Prática 2,
+// fazer:
+// Se o valor da variável SALARIO1 for MAIOR que o valor da
+// variável SALARIO2, então escrever:
+// “O Valor da variável 1 é maior que o valor da variável 2”
+// Caso contrário e o valor da variável SALARIO1 for MENOR
+// que o valor da variável SALARIO2, então escrever, conforme.
+// Caso contrário, escrever: “Os valores são iguais”.
+
+
+if($iSalario1 > $iSalario2){
+    echo "O Valor da variável 1 é maior que o valor da variável 2";
+    } elseif ($iSalario1 < $iSalario2) {
+        echo "O Valor da variável 1 é menor que o valor da variável 2";
+    } else {
+        echo "Os valores são iguais";
+}
+?>
